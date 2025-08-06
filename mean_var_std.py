2719d1623174b1bd36c3af811c5016902c300105
mnyahuma <mnyahuma@gmail.com>
2025-08-06 08:42:00 +0200

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,7 @@ import numpy as np
 def calculate(input_list):
     if len(input_list) != 9:
         raise ValueError("List must contain nine numbers.")
-    
+        
     # Convert list to 3x3 numpy array
     matrix = np.array(input_list).reshape(3, 3)
     
